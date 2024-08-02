2f6e9cf7903acbbd9175cf2f47705103d1d6ff1c
totylkopierdola <kudraj7@wp.pl>
2024-08-02 11:31:49 +0200

    list page + filters

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+// import Filter from "@/components/Filter";
+import Filter from "@/components/Filter";
+import ProductList from "@/components/ProductList";
+// import Skeleton from "@/components/Skeleton";
+import Image from "next/image";
+import { Suspense } from "react";
 
-const ListPage = () => {
-  return <div>ListPage</div>;
+const ListPage = async () => {
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      {/* CAMPAIGN */}
+      <div className="hidden bg-yellow-50 px-4 sm:flex justify-between h-64">
+        <div className="w-2/3 flex flex-col items-center justify-center gap-8">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
+              Grab up to 50% off on
+              <br /> Selected Products
+            </h1>
+            <button className="rounded-3xl bg-yellow-600 text-white w-max py-3 px-5 text-sm">
+              Buy Now
+            </button>
+          </div>
+        </div>
+        <div className="relative w-1/3">
+          <Image
+            src="/images/asianwoman.png"
+            alt=""
+            fill
+            className="object-contain transform scale-x-[-1]"
+          />
+        </div>
+      </div>
+      {/* FILTER */}
+      <Filter />
+      {/* PRODUCTS */}
+      <h1 className="mt-12 text-xl font-semibold">
+        {`cat?.collection?.name`} For You!
+      </h1>
+      {/* <Suspense fallback={<Skeleton />}> */}
+      <ProductList />
+      {/* </Suspense> */}
+    </div>
+  );
 };
 
 export default ListPage;
